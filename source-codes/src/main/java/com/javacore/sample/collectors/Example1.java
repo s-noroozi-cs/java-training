fd7f0678c4dde0fccec7dc7a832e7cd91a71e25d
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-04 13:41:31 +0330

    Create Example1.java

---
+++ source-codes/src/main/java/com/javacore/sample/collectors/Example1.java
@@ -0,0 +1,47 @@
+package com.javacore.sample.collectors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Example1 {
+    record Product(int id, String name, float price) {
+    }
+
+
+    public static void main(String[] args) {
+        List<Product> productsList = new ArrayList();
+        productsList.add(new Product(1, "HP Laptop", 25000f));
+        productsList.add(new Product(2, "Dell Laptop", 30000f));
+        productsList.add(new Product(3, "Lenevo Laptop", 28000f));
+        productsList.add(new Product(4, "Sony Laptop", 28000f));
+        productsList.add(new Product(5, "Apple Laptop", 90000f));
+        productsList.add(new Product(6, "Apple Laptop", 120000f));
+        List<Float> productPriceList =
+                productsList
+                        .stream()
+                        .map(Product::price)
+                        .collect(Collectors.toList());
+        System.out.println(productPriceList);
+
+        Set<String> productNameSet =
+                productsList
+                        .stream()
+                        .map(Product::name)
+                        .collect(Collectors.toSet());
+        System.out.println(productNameSet);
+
+        Double priceSums = productsList.stream().collect(Collectors.summingDouble(Product::price));
+        System.out.println("Sum of prices: " + priceSums);
+
+        Double priceAverage = productsList.stream().collect(Collectors.averagingDouble(Product::price));
+        System.out.println("Average of prices: " + priceAverage);
+
+        Long numberOfElements = productsList.stream().collect(Collectors.counting());
+        System.out.println("Total elements : " + numberOfElements);
+
+
+    }
+
+}
