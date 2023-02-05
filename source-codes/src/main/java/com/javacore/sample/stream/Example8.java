55532c6f89182e30308564dff2656e223e5cd625
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-05 21:24:18 +0330

    Create Example8.java

---
+++ source-codes/src/main/java/com/javacore/sample/stream/Example8.java
@@ -0,0 +1,23 @@
+package com.javacore.sample.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Example8 {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("one");
+        stringList.add("two");
+        stringList.add("three");
+        stringList.add("one");
+
+        Stream<String> stream = stringList.stream();
+
+        stream.limit(2)
+                .forEach(element -> {
+                    System.out.println(element);
+                });
+    }
+}
