739629e2a1ddddd34a9698a0e10cc5db4057c6b6
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-08 19:29:20 +0330

    Create Example2.java

---
+++ source-codes/src/main/java/com/javacore/sample/predicate/Example2.java
@@ -0,0 +1,20 @@
+package com.javacore.sample.predicate;
+
+import java.util.function.Predicate;
+
+public class Example2 {
+    public static void main(String[] args) {
+        Predicate<Integer> greaterThanTen = (i) -> i > 10;
+
+        Predicate<Integer> lowerThanTwenty = (i) -> i < 20;
+        boolean result = greaterThanTen
+                .and(lowerThanTwenty)
+                .test(15);
+        System.out.println(result);
+
+        boolean result2 = greaterThanTen
+                .and(lowerThanTwenty)
+                .negate().test(15);
+        System.out.println(result2);
+    }
+}
