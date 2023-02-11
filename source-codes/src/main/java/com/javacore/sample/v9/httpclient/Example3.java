0787bde90c14480abc09d347162f8328a2cb9d78
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-11 22:05:54 +0330

    Create Example3.java

---
+++ source-codes/src/main/java/com/javacore/sample/v9/httpclient/Example3.java
@@ -0,0 +1,25 @@
+package com.javacore.sample.v9.httpclient;
+
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+public class Example3 {
+    public static void main(String[] args) throws Exception {
+        HttpClient client = HttpClient.newBuilder().build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://www.google.com"))
+                .GET()
+                .build();
+
+        CompletableFuture<HttpResponse<String>> response =
+                client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.get().toString());
+    }
+}
