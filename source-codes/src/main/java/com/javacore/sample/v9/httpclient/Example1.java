15777fc4ae74aa85c480311e3fe7cb3fd8de916e
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-11 21:54:31 +0330

    Create Example1.java

---
+++ source-codes/src/main/java/com/javacore/sample/v9/httpclient/Example1.java
@@ -0,0 +1,20 @@
+package com.javacore.sample.v9.httpclient;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Example1 {
+    public static void main(String[] args) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://www.google.com"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.toString());
+    }
+}
