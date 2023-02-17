95e1803ca3dc1964a25005c186dad9a99c10caf4
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-17 15:03:08 +0330

    Update Example1.java

--- source-codes/src/main/java/com/javacore/sample/v9/trywithresource/Example1.java
+++ source-codes/src/main/java/com/javacore/sample/v9/trywithresource/Example1.java
@@ -5,10 +5,24 @@ import java.io.FileOutputStream;
 
 public class Example1 {
     public static void main(String[] args) throws FileNotFoundException {
-        try(FileOutputStream fileStream=new FileOutputStream("try-with-resource.txt");){
-            String greeting = "Welcome to Java.";
-            byte b[] = greeting.getBytes();
-            fileStream.write(b);
+        //to see affection of these changes,
+        // you should be change project structure configuration
+        // change module java language level
+
+        String filePath = "target/try-with-resource.txt";
+
+        //java 7 syntax
+        try(FileOutputStream fileStream=new FileOutputStream(filePath);){
+            fileStream.write("Welcome to Java Training Course.".getBytes());
+            System.out.println("File written");
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+
+        //java 9 syntax
+        FileOutputStream fileStream=new FileOutputStream(filePath);
+        try(fileStream){
+            fileStream.write("Welcome to Java Training Course.".getBytes());
             System.out.println("File written");
         }catch(Exception e) {
             System.out.println(e);
