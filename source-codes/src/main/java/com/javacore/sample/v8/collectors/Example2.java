6a079e5df35a8517a03ea0177b9c773c033a6e20
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-19 22:06:26 +0330

    Update Example2.java

--- source-codes/src/main/java/com/javacore/sample/v8/collectors/Example2.java
+++ source-codes/src/main/java/com/javacore/sample/v8/collectors/Example2.java
@@ -9,21 +9,29 @@ import java.util.stream.Collectors;
 public class Example2 {
 
     public static void main(String[] args) {
-        List<Product> productsList = Product.getSampleProducts();
+        List<Product> productsList =
+                Product.getSampleProducts();
 
-        Optional<Float> min = productsList.stream().map(Product::price).collect(Collectors.minBy(Float::compareTo));
-        Optional<Float> max = productsList.stream().map(Product::price).collect(Collectors.maxBy(Float::compareTo));
+        Optional<Float> min = productsList.stream()
+                .map(Product::price)
+                .collect(Collectors.minBy(Float::compareTo));
+        Optional<Float> max = productsList.stream()
+                .map(Product::price)
+                .collect(Collectors.maxBy(Float::compareTo));
 
         min.ifPresent(val -> System.out.printf("Minimum price is %.1f\n", val));
         max.ifPresent(val -> System.out.printf("Maximum price is %.1f\n", val));
 
-        Double priceSums = productsList.stream().collect(Collectors.summingDouble(Product::price));
+        Double priceSums = productsList.stream()
+                .collect(Collectors.summingDouble(Product::price));
         System.out.println("Sum of prices: " + priceSums);
 
-        Double priceAverage = productsList.stream().collect(Collectors.averagingDouble(Product::price));
+        Double priceAverage = productsList.stream()
+                .collect(Collectors.averagingDouble(Product::price));
         System.out.println("Average of prices: " + priceAverage);
 
-        Long numberOfElements = productsList.stream().collect(Collectors.counting());
+        Long numberOfElements = productsList.stream()
+                .collect(Collectors.counting());
         System.out.println("Total elements : " + numberOfElements);
     }
 }
