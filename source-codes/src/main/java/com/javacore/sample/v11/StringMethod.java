7b1694883580cde1424c062a5ec786bc768f56fb
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-23 21:19:03 +0330

    Update StringMethod.java

--- source-codes/src/main/java/com/javacore/sample/v11/StringMethod.java
+++ source-codes/src/main/java/com/javacore/sample/v11/StringMethod.java
@@ -2,7 +2,7 @@ package com.javacore.sample.v11;
 
 import java.util.stream.Collectors;
 
-public class StringMethodExample {
+public class StringMethod {
 
     public static void main(String[] args) {
         // is blank
