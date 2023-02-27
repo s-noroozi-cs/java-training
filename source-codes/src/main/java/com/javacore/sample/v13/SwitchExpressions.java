807887c0eb03e90859b073f7eb11552c29810fb1
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-27 21:15:09 +0330

    Update SwitchExpressions.java

--- source-codes/src/main/java/com/javacore/sample/v13/SwitchExpressions.java
+++ source-codes/src/main/java/com/javacore/sample/v13/SwitchExpressions.java
@@ -4,6 +4,20 @@ import java.time.LocalDate;
 
 public class SwitchExpressions {
     public static void main(String[] args) {
+        //Switch expressions were introduced in Java 12.
+        // Break is used here to define return values:
+
+//        int numLetters = switch (LocalDate.now().getDayOfWeek()) {
+//            case MONDAY, FRIDAY, SUNDAY:
+//                break 6;
+//            case TUESDAY:
+//                break 7;
+//            case THURSDAY, SATURDAY:
+//                break 8;
+//            case WEDNESDAY:
+//                break 9;
+//        };
+
         int numLetters = switch (LocalDate.now().getDayOfWeek()) {
             case MONDAY, FRIDAY, SUNDAY:
                 yield 6;
