229c860e291fba54c68bee40be3ff21ec250a2f4
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-01 22:01:29 +0330

    Update Example2.java

--- source-codes/src/main/java/com/javacore/sample/v9/collectionapi/Example2.java
+++ source-codes/src/main/java/com/javacore/sample/v9/collectionapi/Example2.java
@@ -1,7 +1,26 @@
 package com.javacore.sample.v9.collectionapi;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Example2 {
     public static void main(String[] args) {
+        List<String> references = List.of("1", "2", "3");
 
+        System.out.println(references);
+
+        operationA(references);
+
+        System.out.println(references);
+
+    }
+
+    static void operationA(List<String> data) {
+        //x,y,z
+        System.out.println(data.stream()
+                .collect(Collectors.joining(",", "*", "*")));
+        data.add("4");
     }
 }
